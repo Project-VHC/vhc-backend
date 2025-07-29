14b59aeacb51cae6d97766ec0d7e24c15ff63c28
kiranvenkatbeesetty <kiran.b@hiscopetech.com>
2025-07-29 19:23:54 +0530

    added  the pharmacistverificationcontroller

---
+++ src/main/java/com/hiscope/verified_doctor/controller/PharmacistVerificationController.java
@@ -0,0 +1,10 @@
+package com.hiscope.verified_doctor.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PharmacistVerificationController {
+
+
+}
