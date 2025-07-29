75ceb983ec3ec613ccf4c20019d071d21d419636
kiranvenkatbeesetty <kiran.b@hiscopetech.com>
2025-07-29 18:39:10 +0530

    done some changes

--- src/main/java/com/hiscope/verified_doctor/repository/PharmacistVerificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hiscope.verified_doctor.repository;
-
-import com.hiscope.verified_doctor.entity.PharmacistVerification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PharmacistVerificationRepository extends JpaRepository<PharmacistVerification, Long> {
-    PharmacistVerification findByEmail(String email);
-}
